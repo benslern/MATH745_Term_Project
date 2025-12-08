20758cc69ccc9375ce786512b4dfa76da32b8992
benslern <benslern@mcmaster.ca>
2025-12-08 14:19:39 -0500

    final code

--- Code/GCLM_Solver.m
+++ Code/GCLM_Solver.m
@@ -7,23 +7,22 @@ close all;
 
 % Global Constants
 global N; global DT; global H; global T; global F; global X; global K; 
-global L2_MAX; global COUNT; global LINF_SUM; global LINF_MAX;
-N = 2^16;                 % number of grid points
-DT = 0.0001;              % timestep
+global COUNT; global DEALIAS_MAX;
+N = 2^14;                 % number of grid points
+DT = 1E-5;                % timestep
 H = 2*pi/N;               % distance between x_i and x_i+1
 T = 7;                    % timescale
 F = 1E-12;                % filter tolerance
 X = linspace(-pi,pi-H,N); % grid points
 K = [0:N/2-1, -N/2:-1];   % wavenumbers
-L2_MAX = 5E4;
-LINF_SUM = 0;
-LINF_MAX = 1E8;
 COUNT = 1;
+DEALIAS_MAX = 1E-9;
 
 
-% Initial Condition
-eps = 0.1;
-w_0 = sin(X)+eps*sin(2*X);
+% Initial Conditions
+%eps = 0.1;
+%w_0 = sin(X)+eps*sin(2*X);
+%w_0 = exp(2*sin(X));
 SolveGCLM();
 %deriv_test()
 %ht_test()
@@ -70,22 +69,23 @@ function [E] = deriv_test()
     E = norm(dif,2);
 end
 
-
 % Generalized Constantin-Lax-Majda Equation solver
 function SolveGCLM()
-    %a = -0.8;
     global COUNT; global X;
     
     blowup_times = [];
     as = [];
     index = 1;
     
-    for a=0.4:-0.1:0.4
-        COUNT = 1;
+    for a=1.0:-0.1:0.0
         disp("a: "+a);
+        
         eps = 0.1;
         w_0 = sin(X)+eps*sin(2*X);
+        %w_0 = exp(sin(2*X)) - exp(cos(X));
+        
         [~,~,t] = RK4(w_0, a, index); 
+        
         blowup_times(index) = t;
         as(index) = a;
         index = index + 1;
@@ -99,32 +99,35 @@ function SolveGCLM()
     title("Blowup Time vs a");
 end
 
-% Blow-up Checker L2 norm of w_x
-function [b1,n] = blowup_1(w_t)
-    global L2_MAX; global N; global F;
-    n = norm(ifft(deriv(fft(w_t,N),1),N),2);
-    b1 = logical(n>L2_MAX);
+function b = blowup_dealias_checker(w_t)
+    global N; global DEALIAS_MAX; global K;
+    w_c = fft(w_t,N);
+    w_c_high = w_c(K>2*(N/2)/3);
+    val = max(w_c_high);
+    b = logical(val>DEALIAS_MAX);
 end
 
-% Blow-up Checker LINF norm of u_x
-function [b2] = blowup_2(w_t)
-    global N; global LINF_SUM; global LINF_MAX; global F; global DT;
+% L2 norm of w_x
+function n = L2_norm(w_t)
+    global N;
+    n = norm(ifft(deriv(fft(w_t,N),1),N),2);
+end
+
+% LINF norm of u_x
+function n = LINF_norm(w_t)
+    global N;
     n = norm(ifft(ht(fft(w_t,N)),N),Inf);
-    LINF_SUM = LINF_SUM + (n*DT);
-    b2 = logical(LINF_SUM > LINF_MAX);
 end
 
 % Derivative in Fourier Space
 function d = deriv(w_c, ord)
     global K;
-    % multiply by (ik)^ord
     d = ((1i.*K).^ord).*w_c;
 end
 
 % Hilbert Transform
 function h = ht(w_c)
     global K;
-    % multiply Fourier coefficients by -i signum(k)
     h = (-1i*sign(K)).*w_c;
 end
 
@@ -151,58 +154,62 @@ function w_c = dealias(w_c)
 end
 
 % filter |e_k| < 1E-12 to zero
-function w_c = filter(w_c, threshold)
+function w_c = filter(w_c)
+    global F;
     % find points below threshold
-    filter_points = (abs(w_c) <= threshold);
+    filter_points = (abs(w_c) <= F);
     % set filter points to zero
     w_c(filter_points) = 0;
 end
 
-function [f,n] = calc_RHS(w,a,t)
-    global N; global F; global X; global T; global K;
-    w_c = filter(fft(w,N),F); % Convert w to Fourier space
-    ux_c = ht(w_c); % calculate du/dx
-    wx_c = deriv(w_c,1); % calculate dw/dx
-    u = calc_u(w_c); % calculate u
-    w = ifft(w_c,N); %calculate w
+function f = calc_RHS(w,a,t,index)
+    global N;  global X; global T; global K;
+    
+    w_c = fft(w,N); % Convert w to Fourier space  
+    ux_c = ht(w_c); % calculate du/dx in Fourier space
+    wx_c = deriv(w_c,1); % calculate dw/dx in Fourier Space
+    u = calc_u(w_c); % calculate u in physical space
+    w = ifft(w_c,N); %calculate w in physical space
 
     ux = ifft(ux_c,N); % convert du/dx to physical space
     wx = ifft(wx_c,N); % convert dw/dx to physical space
 
-    t1 = ux.*w; % compute du/dx * w
-    t2 = a*(u.*wx); % compute a*u*dw/dx
+    t1 = ux.*w; % compute du/dx * w in physical space
+    t2 = a*(u.*wx); % compute a*u*dw/dx in physical space
 
-    t1 = real(ifft(filter(dealias(fft(t1,N)),F),N)); % dealias t1
-    t2 = real(ifft(filter(dealias(fft(t2,N)),F),N)); % dealias t2
+    %t1 = real(ifft(filter(dealias(fft(t1,N))),N)); % dealias and filter t1
+    %t2 = real(ifft(filter(dealias(fft(t2,N))),N)); % dealias and filter t2
 
     f = t1 - t2;
     
-    n = NaN;
     if(t ~= -1)
-        mod_step = 0.5;
-        if (mod(t,0.001)==0 )
-            [~,n] = blowup_1(w);
-        end
-        if (mod(t,mod_step)==0 && t ~=T)
+        if (mod(t,0.5)==0 && t ~=T)
             disp("t: "+t);
             
             colors = {[0,0,1],[0.5,0.5,0.5]};
             figure(1);
-
             plot(X,real(w),'color', colors{1 + ~(t==0)});
+            filename = sprintf('../Results/w_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+            writematrix(real(w), filename, 'WriteMode', 'append');
             hold on;
         
             figure(2);
-            spec = filter(abs(w_c),F);
+            spec = filter(abs(w_c));
             semilogy(K(K>0),real(spec(K>0)),'color',colors{1 + ~(t==0)});
+            filename = sprintf('../Results/spec_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+            writematrix(real(spec(K>0)), filename, 'WriteMode', 'append');
             hold on;
         
             figure(4);
             plot(X,real(ux),'color',colors{1 + ~(t==0)});
+            filename = sprintf('../Results/ux_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+            writematrix(real(ux), filename, 'WriteMode', 'append');
             hold on;
         
             figure(5);
             plot(X,real(wx),'color',colors{1 + ~(t==0)});
+            filename = sprintf('../Results/wx_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+            writematrix(real(wx), filename, 'WriteMode', 'append');
             hold on;
         end
     end
@@ -210,75 +217,66 @@ end
 
 % Integration of the model problem using the RK4 scheme;
 function [w_T, b, t] = RK4( w_t, a, index)
-    global DT; global X; global T; global N; global F; global K; 
-    global COUNT; global LINF_SUM;
+    global DT; global X; global T; global N; global F; global K; global COUNT; 
     i = 0;
     t = 0;
     b = false;
     COUNT = 1;
-    LINF_SUM = 0;
 
     norm_data = [];
     norm_data_2 = [];
     norm_times = [];
     
-    for j=1:7
+    for j=1:8
         figure(j); clf;
     end
+    calc_RHS(w_t, a, t, index);
 
-    [~,n] = calc_RHS(w_t, a, t);
-    [~] = blowup_2(w_t);
-    norm_data(COUNT) = n;
-    norm_data_2(COUNT) = LINF_SUM;
-    norm_times(COUNT) = t;
-    COUNT = COUNT + 1;
-    disp([n,LINF_SUM]);
-    
     while ( all([(t <= T),(~b)]) )
+
+      norm_data(COUNT) = L2_norm(w_t);
+      norm_data_2(COUNT) = LINF_norm(w_t);
+      norm_times(COUNT) = t;
+      COUNT = COUNT + 1;
+
       t = (i+1) * DT;
      
-      [f,n] = calc_RHS(w_t, a, t);
-      k1 = DT*f;
+      f1 = calc_RHS(w_t, a, t,index);
+      k1 = DT*f1;
 
-      [f,~] = calc_RHS(w_t + k1/2, a, -1);
-      k2 = DT*f;
+      f2 = calc_RHS(w_t + k1/2, a, -1,index);
+      k2 = DT*f2;
       
-      [f,~] = calc_RHS(w_t + k2/2, a, -1);
-      k3 = DT*f;
+      f3 = calc_RHS(w_t + k2/2, a, -1, index);
+      k3 = DT*f3;
       
-      [f,~] = calc_RHS(w_t + k3, a, -1);
-      k4 = DT*f;
-      
+      f4 = calc_RHS(w_t + k3, a, -1, index);
+      k4 = DT*f4;
+
       w_t = w_t + (1/6)*( k1 + 2*k2 + 2*k3 + k4 );
-      
 
-      [b1,~] = blowup_1(w_t);
-      [b2] = blowup_2(w_t);
-      b = logical(b | b1);
-      b = logical(b | b2);
+      b = blowup_dealias_checker(w_t);
+      w_t = real(ifft(filter(dealias(fft(w_t,N))),N)); %filter and dealias w_t
 
-      if(~isnan(n))
-        norm_data(COUNT) = n;
-        norm_data_2(COUNT) = LINF_SUM;
-        norm_times(COUNT) = t;
-        COUNT = COUNT + 1;
-      end
       i = i + 1;
     end
 
     if b
         disp("Blowup");
         disp(t)
+    else
+        disp("DONE");
+        disp(t);
     end
     w_T = w_t;
+    filename = '../Results/blowup_times.csv';
+    writematrix(t, filename, 'WriteMode', 'append');
 
     fg = figure(1);
     figure(fg);
-    if(b)
-        plot(X,real(w_t),"--",'color','r');
-    else
-        plot(X,real(w_t),"-",'color','r');
-    end
+    plot(X,real(w_t),"-",'color','r');
+    filename = sprintf('../Results/w_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(real(w_t), filename, 'WriteMode', 'append');
     title("$\omega$ vs $x$: $a=$" + num2str(round(a,1)), 'Interpreter', 'latex');
     ylabel("$\omega$",'Interpreter','latex');
     xlabel("$x$",'Interpreter','latex');
@@ -291,18 +289,16 @@ function [w_T, b, t] = RK4( w_t, a, index)
 
     fg = figure(2);
     figure(fg);
-    w_c = filter(fft(w_t,N),F);
-    spec = filter(abs(w_c),F);
-    if(b)
-        semilogy(K,real(spec),"--",'color','r');
-    else
-        semilogy(K,real(spec),"-",'color','r');
-    end
+    w_c = filter(fft(w_t,N));
+    spec = filter(abs(w_c));
+    semilogy(K(K>0),real(spec(K>0)),"-",'color','r');
+    filename = sprintf('../Results/spec_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(real(spec(K>0)), filename, 'WriteMode', 'append');
     title("$\omega$ Fourier Spectrum: $a=$" + num2str(round(a,1)), 'Interpreter', 'latex');
-    ylabel("$|\omega_k|$",'Interpreter','latex');
+    ylabel("$|\hat{\omega}_k|$",'Interpreter','latex');
     xlabel("$k$",'Interpreter','latex');
     grid on;
-    xlim([-500,1E4]);
+    xlim([-(N/2)*0.1,(N/2)*1.1]);
     filename = sprintf('../Results/w_spectrum_a%s_%s.jpg', num2str(index), num2str(round(a,1)));
     exportgraphics(fg,filename,'Resolution',600)
     filename = sprintf('../Results/w_spectrum_a%s_%s.fig', num2str(index),num2str(round(a,1)));
@@ -310,11 +306,11 @@ function [w_T, b, t] = RK4( w_t, a, index)
 
     fg = figure(3);
     figure(fg);
-    [~,temp] = blowup_1(w_t);
-    norm_data(COUNT) = temp;
+    norm_data(COUNT) = L2_norm(w_t);
     norm_times(COUNT) = t;
-    val = 0;
-    semilogy(norm_times(1:end-val),norm_data(1:end-val));
+    semilogy(norm_times,norm_data);
+    filename = sprintf('../Results/l2_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(norm_data, filename, 'WriteMode', 'append');
     title("$\|\omega_x\|$ vs Time: $a=$"+ num2str(round(a,1)), 'Interpreter', 'latex');
     ylabel("$\|\omega_x\|$",'Interpreter','latex');
     xlabel("$t$",'Interpreter','latex');
@@ -329,11 +325,9 @@ function [w_T, b, t] = RK4( w_t, a, index)
     figure(fg);
     ux_c = ht(w_c);
     ux = ifft(ux_c,N);
-    if(b)
-        plot(X,real(ux),"--",'color','r');
-    else
-        plot(X,real(ux),"-",'color','r');
-    end
+    plot(X,real(ux),"-",'color','r');
+    filename = sprintf('../Results/ux_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(real(ux), filename, 'WriteMode', 'append');
     title("$u_x$ vs $x$: $a=$" + num2str(round(a,1)),'Interpreter', 'latex');
     ylabel("$u_x$",'Interpreter','latex');
     xlabel("x",'Interpreter','latex');
@@ -348,11 +342,9 @@ function [w_T, b, t] = RK4( w_t, a, index)
     figure(fg);
     wx_c = deriv(w_c,1);
     wx = ifft(wx_c,N);
-    if(b)
-        plot(X,real(wx),"--",'color','r');
-    else
-        plot(X,real(wx),"-",'color','r');
-    end
+    plot(X,real(wx),"-",'color','r');
+    filename = sprintf('../Results/wx_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(real(wx), filename, 'WriteMode', 'append');
     title("$\omega_x$ vs $x$: $a=$" + num2str(round(a,1)),'Interpreter', 'latex');
     ylabel("$\omega_x$",'Interpreter', 'latex');
     xlabel("$x$",'Interpreter', 'latex');
@@ -365,12 +357,12 @@ function [w_T, b, t] = RK4( w_t, a, index)
 
     fg = figure(6);
     figure(fg);
-    temp = blowup_2(w_t)*DT;
-    norm_data_2(COUNT) = temp+LINF_SUM;
-    val = 0;
-    semilogy(norm_times(1:end-val),norm_data_2(1:end-val));
-    title("$\int_0^t \|u_x\|_{\infty}$ vs Time: $a=$" + num2str(round(a,1)), 'Interpreter', 'latex');
-    ylabel("$\int_0^t\|u_x\|_{\infty}$",'Interpreter','latex');
+    norm_data_2(COUNT) = LINF_norm(w_t);
+    semilogy(norm_times,norm_data_2);
+    filename = sprintf('../Results/linf_data_a%s_%s.csv', num2str(index), num2str(round(a,1)));
+    writematrix(norm_data_2, filename, 'WriteMode', 'append');
+    title("$\|u_x\|_{\infty}$ vs Time: $a=$" + num2str(round(a,1)), 'Interpreter', 'latex');
+    ylabel("$\|u_x\|_{\infty}$",'Interpreter','latex');
     xlabel("$t$",'Interpreter','latex');
     grid on;
     xlim([-0.5,7.5]);
@@ -378,5 +370,17 @@ function [w_T, b, t] = RK4( w_t, a, index)
     exportgraphics(fg,filename,'Resolution',600)
     filename = sprintf('../Results/norm2_vs_time_a%s_%s.fig', num2str(index), num2str(round(a,1)));
     savefig(fg,filename)
+
+    %fg = figure(8);
+    %figure(fg);
+    %norm_data_deriv=(norm_data(2:end)-norm_data(1:end-1))/DT;
+    %loglog(norm_data(1:end-1), norm_data_deriv,"color","b");
+    %grid on;
+    %ylabel("dg(t)/dt");
+    %xlabel("g(t)");
+    %filename = sprintf('../Results/dgdt_vs_g_a%s_%s.jpg', num2str(index), num2str(round(a,1)));
+    %exportgraphics(fg,filename,'Resolution',600)
+    %filename = sprintf('../Results/dgdt_vs_g_a%s_%s.fig', num2str(index), num2str(round(a,1)));
+    %savefig(fg,filename)
 
 end
